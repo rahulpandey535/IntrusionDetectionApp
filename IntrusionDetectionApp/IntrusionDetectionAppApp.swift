bacbbf9cf0854ed999d1723e9c572c492469d068
Rahul Pandey <rahulpandey02124@gmail.com>
2024-12-19 10:23:05 +0530

    Initial Commit

---
+++ IntrusionDetectionApp/IntrusionDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntrusionDetectionAppApp.swift
+//  IntrusionDetectionApp
+//
+//  Created by Rahul Pandey on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct IntrusionDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntrusionDetectionAppTests/IntrusionDetectionAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  IntrusionDetectionAppTests.swift
+//  IntrusionDetectionAppTests
+//
+//  Created by Rahul Pandey on 12/19/24.
+//
+
+import Testing
+
+struct IntrusionDetectionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
